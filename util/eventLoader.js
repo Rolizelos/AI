07f25817a80c95d3228667d97bd19332eb1187e3
Glitch (responsible-tuna) <none>
2019-11-16 14:06:31 +0000

    💾🎅 Checkpoint ./util/eventLoader.js:3251742/70

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -1,4 +1,6 @@
 const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
-  client.on('message', reqEvent('message'));
+  client.on('message', reqEvent('message'))
+ client.on('ready', () => reqEvent('ready')(client));
 };
+
