a22f79485c85e0094a9cdbb0d38d5258c35a3aa4
Glitch (responsible-tuna) <none>
2019-12-11 19:23:42 +0000

    🙍🎪 Checkpoint ./komutlar/js.js:8072212/2169

--- komutlar/js.js
+++ komutlar/js.js
@@ -2,11 +2,17 @@ const Discord = require('discord.js');
 
 
 exports.run = (client, message, args) => {
+  
+  if (message.channel.id !== '654376897412595723') return message.channel.send(`${client.emojis.get("648206354904907776")} \`${message.author.tag}\`**Bu Komutudu <#654376897412595723> Kanalında Kullan!**`).then(msg => {
+    msg.delete(10000)
+    })
+  
    message.member.addRole("654395894208397331") 
    const embed = new Discord.RichEmbed()
    .setColor('RED')         
 .setTitle('Başarılı : :white_check_mark: ')
 .setDescription(`${message.author.tag} Javascript Rolü Başarıyla Verildi`)
+.setFooter(`CoderLab | CoderLab`)
 return message.channel.send(embed);
   }
 
