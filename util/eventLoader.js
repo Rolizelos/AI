2541250ffd1714f3d2f70d5e00101ab4766ba5f8
Glitch (responsible-tuna) <none>
2019-12-11 19:00:19 +0000

    🍱🚍 Checkpoint ./events/guildMemberAdd.js:8072212/644 ./util/eventLoader.js:8072212/51

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -2,6 +2,5 @@ const reqEvent = (event) => require(`../events/${event}`);
 module.exports = client => {
   client.on('message', reqEvent('message'))
   client.on('ready', () => reqEvent('ready')(client));
-  client.on('guildMemberAdd', reqEvent('ototag'));
 };
 
