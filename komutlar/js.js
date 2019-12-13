9b582af8f4035b9d6a0dfe067854c8982e8ae384
Glitch (responsible-tuna) <none>
2019-12-13 18:04:00 +0000

    🍇🛋 Checkpoint ./komutlar/ping.js:8072212/2 ./komutlar/js.js:8072212/3044

--- komutlar/js.js
+++ komutlar/js.js
@@ -1,29 +1,45 @@
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
+const DBL = require('dblapi.js')
+exports.run = function(client, message) {
+
+  
 
 
-exports.run = (client, message, args) => {
   
-  if (message.channel.id !== '654376897412595723') return message.channel.send(`:x: \`${message.author.tag}\`**Bu Komutudu <#654376897412595723> Kanalında Kullan!**`).then(msg => {
-    msg.delete(10000)
-    })
+/////////////////////Config
+
+ var logkanali = "true"; //log kanalı kullanılacak ise true yapın kullanılmayacaksa false yapın
+ var rolismi = "JavaScript" // Verilecek Rol Ismi
+var logkanalid = "654378716016672805"   
+let kanal = "650304608371081216"
+  var guildid = "654375129282904075" // Kullanılacak Sunucu
+
+///////////////// Ana Kod
+
+  var role = message.guild.roles.find(i => i.name === rolismi); 
+  if (message.member.roles.has(role.id)) return message.channel.send("❌ Maalesef, Bu Role Sahipsin")
+  if(!role) return message.channel.send(rolismi + "Adından Bir Rol Bulamıyorum Lütfen <@"+ayarlar.sahip+"> 'e ulaş" )
+  if(logkanali == "true") {
+message.member.addRole(role);
+message.channel.send("✅  Başarıyla `JavaScript` Rolün verildi!")
+client.channels.get(logkanalid).send("✅  | " + message.author +" Adlı Kullanıcı Başarılı Bir Şekilde **"+rolismi+"** Adlı Rolü Aldı!")
+}else {
+message.channel.send('✅  Başarıyla `JavaScript` Rolün verildi!')
+message.member.addRole(role);
+}
   
-   message.member.addRole("654395894208397331") 
-   const embed = new Discord.RichEmbed()
-   .setColor('RED')         
-.setTitle('Başarılı : :white_check_mark: ')
-.setDescription(`${message.author.tag} Javascript Rolü Başarıyla Verildi`)
-.setFooter(`CoderLab | CoderLab`)
-return message.channel.send(embed);
-  }
+    };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["js"]
+  aliases: ['javascript', 'de'],
+  permLevel: 0
 };
 
 exports.help = {
   name: 'js',
-  description: 'Send a Minecraft Achievement image to the channel',
-  usage: 'mcödül Title|Text (/achievement Achievement Get|Used a Command!)'
+  description: 'JavaScript Rolünü Verir',
+  usage: 'js'
 };
