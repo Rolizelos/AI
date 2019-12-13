32d8380c622ed79f58e66884e76ec6cc52e743b6
Glitch (responsible-tuna) <none>
2019-12-13 17:52:00 +0000

    😹🏖 Checkpoint ./bot.js:8072212/352 ./komutlar/ototag.js:8072212/3261 ./komutlar/ping.js:8072212/325

--- bot.js
+++ bot.js
@@ -125,4 +125,14 @@ client.on("message", async msg => {
     }
   }
 });
+
+//OTO TAG ---------------------------------------------------------
+
+client.on('guildMemberAdd', async member => {
+  let cfxtag2 = await db.fetch(`cfxtag${member.guild.id}`)
+  var cfxtag = [];
+  if(cfxtag2 == null) cfxtag = `${member.user.username}`
+  else cfxtag = `${cfxtag2} ${member.user.username}`  
+  member.setNickname(`${cfxtag}`)
+});
 client.login(ayarlar.token);
