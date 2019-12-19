ab608fc6698f63f08cd2572ef5168b27bfeb45ac
Glitch (responsible-tuna) <none>
2019-12-19 17:37:55 +0000

    🙆🎖 Checkpoint ./bot-pp.js:8072212/12 ./komutlar/bot-pp.js:8072212/883

---
+++ komutlar/bot-pp.js
@@ -0,0 +1,25 @@
+const Discord = require(`discord.js`);
+
+exports.run = async (client, message, args) => {
+
+const bergy = new Discord.RichEmbed()
+.setColor(`RANDOM`)
+.setDescription(`Botun PP'si ! `)
+.setImage(client.user.avatarURL)
+
+return message.channel.send(bergy).then(n => n.delete(40000));
+  
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: ['y2'],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'bot-pp',
+    description: 'GELİŞMİŞ MENÜLÜ YARDIM',
+    usage: 'yardım2'
+  };
