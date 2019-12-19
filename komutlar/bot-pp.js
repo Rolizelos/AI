c42c9000e2581f09ed3dd1f7a6a71e353afa08f8
Glitch (responsible-tuna) <none>
2019-12-19 17:49:55 +0000

    🍍🚠 Checkpoint ./komutlar/bot-pp.js:8072212/79

--- komutlar/bot-pp.js
+++ komutlar/bot-pp.js
@@ -4,7 +4,7 @@ exports.run = async (client, message, args) => {
 
 const bergy = new Discord.RichEmbed()
 .setColor(`RANDOM`)
-.setDescription(`Botun PP'si ! `)
+.setDescription(`Botun PP'si !`)
 .setImage(client.user.avatarURL)
 
 return message.channel.send(bergy).then(n => n.delete(40000));
@@ -20,6 +20,8 @@ exports.conf = {
   
   exports.help = {
     name: 'bot-pp',
-    description: 'GELİŞMİŞ MENÜLÜ YARDIM',
-    usage: 'yardım2'
+    description: 'Bot Avatar!',
+    usage: 'bot-pp'
   };
+
+//By Bergy | LiberCode/Botlist
