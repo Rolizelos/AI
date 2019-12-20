6d737d60da83f01ff8a918d0d526b6f04463b182
Glitch (responsible-tuna) <none>
2019-12-20 16:15:39 +0000

    👆🎩 Checkpoint ./bot.js:8072212/4

--- bot.js
+++ bot.js
@@ -109,7 +109,7 @@ client.elevation = message => {
 client.on("message", async msg => {
   if (msg.channel.type === "dm") return;
   if (msg.author.bot) return;
-  if (msg.content.length >0) {
+  if (msg.content.length >2) {
     if (db.fetch(`capslock_${msg.guild.id}`)) {
       let caps = msg.content.toUpperCase();
       if (msg.content == caps) {
