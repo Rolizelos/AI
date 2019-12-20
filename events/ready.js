df8b5690e2dae8ec531c2e00bfdf7b3352e3a41d
Glitch (responsible-tuna) <none>
2019-12-20 11:30:07 +0000

    👞🐸 Checkpoint ./komutlar/istek.js:8072212/229 ./events/ready.js:8072212/17

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
 }, 8000);
 client.user.setPresence({
         game: {
-            name: `!js | CoderLab`,
+            name: `!js | LiberCode`,
             type: 'watching'
         },
         status: 'online'
