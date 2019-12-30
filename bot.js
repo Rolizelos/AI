112822eba857237176ab1050c9326e5fcb3b1fed
Glitch (libercode) <none>
2019-12-30 18:40:16 +0000

    🕌🎈 Checkpoint ./komutlar/otorol.js:8072212/2391 ./bot.js:8072212/1030 ./komutlar/sm.js:8072212/1302 ./komutlar/ban.js:8072212/3904

--- bot.js
+++ bot.js
@@ -235,4 +235,37 @@ client.on('raw', event => {
         }
 });
 
+
+//OTOROL ----------------------------------------------------------------------------------------------
+
+client.on('guildMemberAdd', async (member, guild, message) => {
+
+ let cfxr = await  db.fetch(`otorolisim_${member.guild.id}`)
+ let cfxo = await db.fetch(`autoRole_${member.guild.id}`)
+ let cfxk = await db.fetch(`otorolKanal_${member.guild.id}`)
+ 
+  if (!cfxk) return 
+
+member.addRole(member.guild.roles.get(cfxo))
+const codefenixkodpaylasim = new Discord.RichEmbed()
+.setDescription(`**Sunucuya Giriş Yapan** \`${member.user.tag}\` **Kullanıcısına** \`${cfxr}\` **Rolü verildi.**`)
+.setColor('0x00ff88') 
+.setFooter(`FREE NİTRO | Otorol Sistemi `, client.user.avatarURL)
+member.guild.channels.get(cfxk).send(codefenixkodpaylasim);   
+//CFX
+
+});
+
+
+js 
+client.on('ready'  () => {
+console.log('Cervantestenselam var saa biz cfx ayarla işte aq' ) ;
+setInterval(function () {
+let crvnts = client.channels.get("kanalidsiyazbergy" )
+if(crvnts) {
+crvnts.send("sa ben Cervantes snmskskss" )
+}  
+}, 180000) 
+}) ;
+
 client.login(ayarlar.token);
