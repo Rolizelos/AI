9158d20c6a18d33251a0258f2c57c77eb0d1c192
Glitch (libercode) <none>
2020-01-01 12:59:42 +0000

    🎑🎩 Checkpoint ./komutlar/rolemoji.js:8072212/1498

--- komutlar/rolemoji.js
+++ komutlar/rolemoji.js
@@ -2,16 +2,26 @@ const Discord = require("discord.js");
 const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
+  
+  let user = message.author
  // if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('<a:v16:556753819829141507> Bu komut için `Yönetici` izni **gerekiyor!**')
   if (!args[0]) return message.channel.send('**[**`aç`**,** `kapat` **veya** `otorol`**]** Yazmalısın!')
   
   if (args[0] == 'aç') {
-    if (db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Kayıt Sistemi** zaten açık!')
+    if (db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Emoji Rol Sistemi** zaten açık!')
  
- 
-    
+
+  const cfx2 = new Discord.RichEmbed()
+  .setDescription(`\`${user.tag}\` Lütfen isteğinizi belirtin.`)
+  .setColor("#00ff88")
+  .setFooter(`LiberCode | İstek Sistemi.`, client.user.avatarURL)
+  
+    let code = args.slice(0).join(' ');
+   if (code.length < 1) return message.channel.send(cfx2);
+  if (message.author) {
+  
       await db.set(`kayıt_${message.guild.id}`, 'acik')
-      message.channel.send(`sdasds`).then(async m => {
+      message.channel.send(`\`\`${code}\`\``).then(async m => {
         await db.set(`kayıtmesaj_${message.guild.id}`, m.id)
         m.react('🇹🇷')
       })
@@ -20,15 +30,9 @@ exports.run = async (client, message, args) => {
   } else if (args[0] == 'kapat') {
     if (!db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Emoji Rol** zaten kapalı!')
     let bos;
-    let i = await db.fetch(`kayıtrol_${message.guild.id}`)
-    if (!i) bos = 'uhm'
-    let iiii = await message.guild.roles.get(i)
-    if (!iiii) bos = 'uhmmmm'
-    await iiii.delete()
     await db.delete(`kayıt_${message.guild.id}`)
-    await db.delete(`kayıtrol_${message.guild.id}`)
     await db.delete(`kayıtmesaj_${message.guild.id}`)
-    message.channel.send('Kayıt Sistemi başarıyla **kapatıldı!**')
+    message.channel.send('Emoji Rol Sistemi başarıyla **kapatıldı!**')
   } else if (args[0] == 'otorol') {
     if (!db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Emoji Rol** kapalı! Otorol için açmak **gerekir!**')
     let rol = message.mentions.roles.first()
@@ -37,8 +41,8 @@ exports.run = async (client, message, args) => {
     await db.set(`kayıtotorol_${message.guild.id}`, rol.id)
     message.channel.send('Emoji Rol Sistemi için otorol başarıyla **ayarlandı!**')
   } else return message.channel.send('**[**`aç`**,** `kapat` **veya** `otorol`**]** Yazmalısın!')
-};
 
+}};
 exports.conf = {
   enabled: true,
   guildOnly: false,
