11e7cbe5256c71bb56a1f01ae196d04bfbdb0820
Glitch (libercode) <none>
2020-01-01 13:23:43 +0000

    🎽🎖 Checkpoint ./komutlar/rolemoji.js:8072212/1529

--- komutlar/rolemoji.js
+++ komutlar/rolemoji.js
@@ -5,12 +5,11 @@ exports.run = async (client, message, args) => {
   
   let user = message.author
  // if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('<a:v16:556753819829141507> Bu komut için `Yönetici` izni **gerekiyor!**')
-  if (!args[1]) return message.channel.send('**[**`aç`**,** `kapat` **veya** `otorol`**]** Yazmalısın!')
   
   const cfx2 = new Discord.RichEmbed()
-  .setDescription(`\`${user.tag}\` Lütfen isteğinizi belirtin.`)
+  .setDescription(`\`\`${user.tag}\`\` **Emoji Rol Komutları;** \`\`emoji aç/kapat/otorol\`\` **Emoji Rol Sistemini Açmak İçin; \`\`emoji aç 'açıklama'\`\`** `)
   .setColor("#00ff88")
-  .setFooter(`LiberCode | İstek Sistemi.`, client.user.avatarURL)
+  .setFooter(`Emoji Rol Sistemi`, client.user.avatarURL)
   
     let code = args.slice(1).join(' ');
    if (code.length < 1) return message.channel.send(cfx2);
@@ -21,6 +20,7 @@ exports.run = async (client, message, args) => {
         await db.set(`kayıtmesaj_${message.guild.id}`, m.id)
         m.react('🇹🇷')
       })
+
       message.channel.send('Başarıyla **Emoji Rol Sistemi** kuruldu!')
   
   if (args[2] == 'aç') {
@@ -33,22 +33,17 @@ exports.run = async (client, message, args) => {
       })
       message.channel.send('Başarıyla **Emoji Rol Sistemi** kuruldu!')
     
-  } else if (args[0] == 'kapat') {
-    if (!db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Emoji Rol** zaten kapalı!')
-    let bos;
-    await db.delete(`kayıt_${message.guild.id}`)
-    await db.delete(`kayıtmesaj_${message.guild.id}`)
-    message.channel.send('Emoji Rol Sistemi başarıyla **kapatıldı!**')
-  } else if (args[0] == 'otorol') {
+  } 
+  } else if (args[] == 'otorol') {
     if (!db.has(`kayıt_${message.guild.id}`)) return message.channel.send('Bu sunucuda **Emoji Rol** kapalı! Otorol için açmak **gerekir!**')
     let rol = message.mentions.roles.first()
     if (!rol) return message.channel.send('Lütfen bir rol **etiketleyin!**')
     
     await db.set(`kayıtotorol_${message.guild.id}`, rol.id)
     message.channel.send('Emoji Rol Sistemi için otorol başarıyla **ayarlandı!**')
-  } else return message.channel.send('**[**`aç`**,** `kapat` **veya** `otorol`**]** Yazmalısın!')
+  }
 
-}};
+};
 exports.conf = {
   enabled: true,
   guildOnly: false,
