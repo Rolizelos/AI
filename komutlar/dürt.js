55a9008e5f2605071f7bae73b25ff4a5aaec82e9
Glitch (libercode) <none>
2020-01-04 21:12:14 +0000

    👩😬 Checkpoint ./komutlar/mute.js:8072212/4146 ./komutlar/dürt.js:8072212/73 ./komutlar/ban.js:8072212/326

--- komutlar/dürt.js
+++ komutlar/dürt.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+
 exports.run = (client, message, args) => {
       
    if (!message.guild) {
--- komutlar/mute.js
+++ komutlar/mute.js
@@ -1,13 +1,18 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 const ayarlar = require("../ayarlar.json")
+const client = new Discord.Client();
 //CodeFENIX
 exports.run = async (bot, message, args) => {
+ 
+   /////////////////////Config
 
+ var logkanali = "true"; //log kanalı kullanılacak ise true yapın kullanılmayacaksa false yapın
+var logkanalid = "663124687450275860"   //Log Kanalı Id
+  var guildid = "658191439527936001" // Kullanılacak Sunucu
+///////////////// Ana Kod
 
-  
-  //CodeFENIX, JaimiTR Tarafından Kodlanmıştır.//CFX
-  
+    
   let susturulan = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   const cfxhata = new Discord.RichEmbed()
   .setDescription(`**Hatalı Kullanım! \n\n **\`${ayarlar.prefix}mute <@susturalacak-kisi> <süre(1s,1m,1d,1h)>\``)
@@ -44,6 +49,8 @@ let susturulanrol = message.guild.roles.find(susturulanrol => susturulanrol.name
 //CodeFENIX  
 //CodeFENIX
   let susturmazamani = args[1];
+
+  
   const cfxsure = new Discord.RichEmbed()
   .setDescription(`**Süresini Belirtmelisiniz.**`)
   .setColor("#d90000")
@@ -52,10 +59,13 @@ let susturulanrol = message.guild.roles.find(susturulanrol => susturulanrol.name
 //CodeFENIX  
   await(susturulan.addRole(susturulanrol.id));
   const cfxsusturulan = new Discord.RichEmbed()
+  
   .setDescription(`<@${susturulan.id}> \`${ms(ms(susturmazamani))}\` **süre boyunca susturma cezası aldı! **`)
   .setColor("#0088ff")
   .setFooter(`Susturan Yetkili --> ${message.author.username}`)
+  client.channels.get(logkanalid).send(`✅  | ${user.tag} **Adlı Kullanıcı Sunucudan Banlandı!** \n  **Reason=>** \`\ ${reason} \`\ `)}
 message.channel.sendEmbed(cfxsusturulan)
+  
 //CodeFENIX
   setTimeout(function(){
     susturulan.removeRole(susturulanrol.id);
@@ -65,7 +75,8 @@ message.channel.sendEmbed(cfxsusturulan)
   .setFooter(`Susturan Yetkili --> ${message.author.username}`)
   message.channel.sendEmbed(susturmasonu)
   }, ms(susturmazamani));
-}
+  
+}};
 //CodeFENIX
 exports.conf = {
   enabled: true,
