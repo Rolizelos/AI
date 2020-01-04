28b03c10947f095666cef2c0c524ab715bac2d25
Glitch (libercode) <none>
2020-01-04 19:14:05 +0000

    👋🎁 Checkpoint ./komutlar/dürt.js:8072212/207

--- komutlar/dürt.js
+++ komutlar/dürt.js
@@ -3,19 +3,16 @@ exports.run = (client, message, args) => {
       
    if (!message.guild) {
   return message.reply(':no_entry_sign: Bu komut, özel mesajlarda kullanılamamakta!'); }
- if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-    return message.reply(':fire: Yeterli yetki, bulunmakata!');
-  }
   let guild = message.guild;
   let user = message.mentions.users.first();
-  if (message.mentions.users.size < 1) return message.reply('Kimi uyarmam gerekiyor?').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('Kimi Dürtmem Gerekiyor!').catch(console.error);
   message.delete();
-  message.reply('Kişi, başarılı bir şekilde uyarıldı!')
+  message.reply('Kişi, başarılı bir şekilde Dürtüldü!')
   const bergy = new Discord.RichEmbed()
   .setColor('RANDOM')
   .setTitle(`**FaceBook Dürtülme Sistemi!**`)
   .setTimestamp()
-  .setDescription(`**\`${user.tag}\` Sizi Dürttü!**`)
+  .setDescription(`**\`${message.tag.author}\` Sizi Dürttü!**`)
   .setFooter('Facebook Dürt Sistemi', client.user.avatarURL)
   return user.send(bergy);
 };
@@ -24,7 +21,7 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: ['pm','öm'],
-  permlevel: 4
+  permlevel: 0
 };
 
 exports.help = {
