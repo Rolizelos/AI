b73a2b9efc8b89a57bc9644f27b90323e6fa8207
Glitch (libercode) <none>
2020-01-05 20:28:04 +0000

    🐇🌗 Checkpoint ./bot.js:8072212/821

--- bot.js
+++ bot.js
@@ -200,24 +200,10 @@ client.on('raw', event => {
 
 //OTOROL ----------------------------------------------------------------------------------------------
 
-client.on('guildMemberAdd', async (member, guild, message) => {
-
- let cfxr = await  db.fetch(`otorolisim_${member.guild.id}`)
- let cfxo = await db.fetch(`autoRole_${member.guild.id}`)
- let cfxk = await db.fetch(`otorolKanal_${member.guild.id}`)
- 
-  if (!cfxk) return 
-
-member.addRole(member.guild.roles.get(cfxo))
-const codefenixkodpaylasim = new Discord.RichEmbed()
-.setDescription(`**Sunucuya Giriş Yapan** \`\`${member.user.tag}\`\` **Kullanıcısına** \`\`${cfxr}\`\` **Rolü verildi.**`)
-.setColor('0x00ff88') 
-.setFooter(`LiberCode | Otorol Sistemi `, client.user.avatarURL)
-member.guild.channels.get(cfxk).send(codefenixkodpaylasim);   
-//CFX
-
-});
-
+client.on("guildMemberAdd", member => {
+  var rol = member.guild.roles.get("662009787470315544")
+   member.addRole(rol)
+   })
 //------------------------------------------------------------------------------
 
 
