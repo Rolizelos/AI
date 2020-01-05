8778e733026a655d74af2de97380a34a1c6dddee
Glitch (libercode) <none>
2020-01-05 11:50:14 +0000

    👴🐮 Checkpoint ./komutlar/ban.js:8072212/123

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
+  
+  if (!message.member.roles.has("663347155787841546")) return message.channel.send(`Bu komutu kullanmaya yetkin yok!`);
 
   /////////////////////Config
 
