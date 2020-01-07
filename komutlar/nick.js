1b69f639cf2e1f733dd628cca01dc96af7b00ae2
Glitch (libercode) <none>
2020-01-07 20:17:50 +0000

    🎂🐢 Checkpoint ./komutlar/nick.js:8072212/1803

--- komutlar/nick.js
+++ komutlar/nick.js
@@ -5,20 +5,30 @@ exports.run = async (client, message, args) => {
     return message.channel.send(
       `❌ Bu Komutu Kullanabilmek için \`İsimleri Yönet\` Yetkisine Sahip Olmalısın!`
     );
+  
+  let code = args.slice(0).join(' ');
+    if (code.length < 1) return message.channel.send(cfx2);
+  
+  const cfx2 = new Discord.RichEmbed()
+  .setDescription(`\`${user.tag}\` Lütfen isteğinizi belirtin.`)
+  .setColor("#00ff88")
+  .setFooter(`LiberCode | İstek Sistemi.`, client.user.avatarURL)
+  
   let member = message.mentions.members.first();
+  let user = message.mentions.users.first();
   let isim = args.slice(1).join(" ");
-  if (!member) return message.channel.send("❌ Bir Üye Etiketlemelisin!");
   if (!isim) return message.channel.send("❌ Bir İsim Yazmalısın!");
   member.setNickname(`${isim}`);
   const embed = new Discord.RichEmbed()
     .addField(
       `**🏷 İsim Değiştirildi 🏷**`,
-      `\n \n**🔸️İsmi Değiştirilen Kullanıcı:** ${member.user} \n🔸️ **Yeni Kullanıcı Adı:** \`${isim}\``
+      `\n \n**🔸️İsmi Değiştirilen Kullanıcı:** ${user.tag} \n🔸️ **Yeni Kullanıcı Adı:** \`${isim}\``
     )
     .setFooter(`Liber Code | Nick Sistemi`)
     .setThumbnail(client.user.avatarURL);
   message.channel.send(embed);
 };
+
 
 exports.conf = {
   enabled: true,
