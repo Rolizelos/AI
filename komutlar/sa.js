f204ae63fac8ad027a4454037bb42a645542ae29
Glitch (libercode) <none>
2020-01-14 16:13:01 +0000

    🚂👚 Checkpoint ./komutlar/sa.js:8072212/1282

--- komutlar/sa.js
+++ komutlar/sa.js
@@ -1,27 +1,26 @@
 const Discord = require('discord.js');
 const db = require('quick.db')
 const client = new Discord.Client();
-exports.run = async (bot, message, args) => {
+exports.run = async (client, message, bot, args) => {
   
   /////////////////////Config
-
- var logkanali1 = "true"; //log kanalı kullanılacak ise true yapın kullanılmayacaksa false yapın
-var logkanalid1 = "666670859439702026"   //Log Kanalı Id
-  var guildid = "665929879199416320" // Kullanılacak Sunucu
+ var logkanali1 = "true"; //LOG KANALI KULLANCILACAK İSE TRUE YAPIN KULLANILMAYACAK İSE FALSE YAPIN!
+var logkanalid1 = "LOG KANALI ID"   //Log KANALININ IDSİ
+  var guildid = "SUNUCU ID" // KULLANILACAK SUNUCU IDSİ
 ///////////////// Ana Kod
   
   if (!message.member.roles.find('name', 'Yönetici')) return message.channel.send('Dostum Bu Komutu Kullanabilmek İçin **Yönetici** Yetkisine Sahip Olman Gerek');
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!user) return message.reply("**Dostum Etiket Atmayı Unuttun**");
 const alinacak = message.guild.roles.find('name', 'Kayıtsız Üye');
-const verilecek = message.guild.roles.find('name', 'Üye');
+const verilecek = message.guild.roles.find('name', 'Erkek');
 //-------------------------------------------------------------------
 if(logkanali1 == "true") {
-message.guild.channels.find(x => x.id === logkanalid1).send(`${client.emojis.get("EMOJİ ID")} ${user.tag} **Sunucumuza Başarıyla Kayıt Oldu!**\n **${user.tag} \`\`${message.guild.name}\`\`\ Sunucusuna Hoşgeldin!**`)}
+message.guild.channels.find(x => x.id === logkanalid1).send(`${client.emojis.get("EMOJİ ID")} ${user} **Sunucumuza Başarıyla Kayıt Oldu!**\n **${user} \`\`${message.guild.name}\`\`\ Sunucusuna Hoşgeldin!**`)}
 //-------------------------------------------------------------------
 const ky = new Discord.RichEmbed()
         .setAuthor(message.author.tag, message.author.avatarURL)
-        .setDescription(`**${user}, Kaydını Tamamladım ! İyi Eğlenceler   :asoult_tik: \n **`)
+        .setDescription(`**${user}, Kaydını Tamamladım ! İyi Eğlenceler ${client.emojis.get("EMOJİ ID")}**`)
         .setColor('BLACK')
         .setTimestamp()
         message.channel.send(ky)
@@ -42,3 +41,5 @@ exports.help = {
     description: 'erkek',
     usage: 'erkek'
 }
+//BY BERGY 
+//ÖNEMLİ NOTLAR: 'Kayıtsız Üye' Ve 'Erkek' Rollerinizin Olması Lazımdır! Komutu Kullanabilmek İçinde 'Yönetici' Rolünün Olması Lazımdır!
