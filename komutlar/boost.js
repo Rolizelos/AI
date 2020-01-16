ece63c8714faf8eb89b215fe266ef54afa4a2155
Glitch (libercode) <none>
2020-01-16 12:11:25 +0000

    🍈🍿 Checkpoint ./komutlar/boost.js:8072212/113

--- komutlar/boost.js
+++ komutlar/boost.js
@@ -4,24 +4,24 @@ const db = require('quick.db')
 const ayarlar = require('../ayarlar.json')
 
 exports.run = async (client, message ,args) => {
-    if (!args[0]) return message.channel.send(`${client.emojis.get("665930842786365473")} **Eksik Veya Hatalı Komut Kullanımı:** Doğr \`${ayarlar.prefix}reklamkoruma aç/kapat\``)
+    if (!args[0]) return message.channel.send(`${client.emojis.get("665930842786365473")} **Eksik Veya Hatalı Komut Kullanımı:** Doğru Kullanım: \`${ayarlar.prefix}hgkanal ayarla #kanal /${ayarlar.prefix}hgkanal kapat\``)
   
   let cfxkanal = message.mentions.channels.first()
   if(args[0] == 'ayarla') {
   db.set(`cfxdbayar${message.guild.id}`, cfxkanal.id)
-  message.channel.send(`**Kanal Ayarlandı! \n Ayarlanan Kanal:** ${cfxkanal.name}`)
+  message.channel.send(`**Kanal Ayarlandı! \n Ayarlanan Kanal:** \`${cfxkanal.name}\``)
     return;
   }
   if(args[0] == 'kapat') {
   db.delete(`cfxdbayar${message.guild.id}`)
-  message.channel.send(`**Hoşgeldin Kanalı Sıfırlandı!**`)
+  message.channel.send(`**Hoşgeldin Kanalı Kapatıldı!**`)
     return;
   }
 }
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['hg',],
   permLevel: 0,
 };
   //CodeFENIX //CFX
