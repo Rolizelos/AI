1d39f9034d04be435bc96dcee4b02b3ec5bbef87
Glitch (libercode) <none>
2020-01-17 10:35:49 +0000

    🍓🗃 Checkpoint ./komutlar/s.js:8072212/1516

--- komutlar/s.js
+++ komutlar/s.js
@@ -1,39 +1,24 @@
-const Discord = require("discord.js");
-let ayarlar = require("../ayarlar.json");
-let prefix = ayarlar.prefix;
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var randomstring = require('randomstring');
 
-exports.run = function(client, msg, author) {
-  let yazi = msg.mentions.members.first();
-  if (!yazi) {
-    msg.channel.sendMessage(
-      "**Örnek Kullanım:** `" +
-        prefix +
-        "sakso <@kullanıcı>`"
-    );
-  }
-  if (yazi) {
-    const Embed = new Discord.RichEmbed()
-      .setTitle("")
-      .setDescription(
-        `Ohh ❤ ** <@${msg.author.id}> Adlı Kullanıcı ${yazi} Adlı Kullanıcıya Sakso Çekti!**`
-      )
-      .setColor("RANDOM")
-      .setImage(
-        "https://cdn.discordapp.com/attachments/657271597320896522/664499852172066877/liseli-kiza-sakso-cektirip-sikme_1.gif"
-      );
-    msg.channel.sendMessage(Embed);
-  }
+exports.run = (client, message) => {
+     message.channel.send(
+            randomstring.generate({
+            length: 12,
+            charset: 'alphabetic'
+}))
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
+  aliases: ['rastgeleşifre', 'rastgeleşifre', 'rastgeleşifre', 'rastgeleşifre'],
   permLevel: 0
 };
 
 exports.help = {
-  name: "sakso",
-  description: "Bu komutla istediğine Sakso Çektirirsin!",
-  usage: "sakso <@kullanıcı>"
+  name: 'rastgeleşifre',
+  description: 'random şifre atar',
+  usage: 'rastgeleşifre'
 };
