01fa8729151b210a2a7d0ebc0da8267eb100dc2b
Glitch (libercode) <none>
2020-01-17 15:19:30 +0000

    🏹💨 Checkpoint ./komutlar/say.js:8072212/997

--- komutlar/say.js
+++ komutlar/say.js
@@ -2,21 +2,28 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
   const bergy = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setDescription(`**Sunucuda ${message.guild.memberCount} Kişi Bulunmaktadır!**`)
-    .setAuthor("Toplam Üye", `${message.author.displayAvatarURL}`)
+.setColor("RANDOM")
+.setAuthor(message.guild.name, message.guild.iconURL)
+.setThumbnail(message.guild.iconURL)  
+.setDescription(`**Sunucudaki Toplam Üye Sayısı:** \`${message.guild.memberCount}\`
+**Sunucudaki Offline Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'offline').size}\`
+**Sunucudaki Online Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'online').size}\`
+**Sunucudaki Bot Sayısı:** \`${message.guild.members.filter( member => member.user.bot).size}\`
+`)
+.setFooter(message.guild.iconURL,)
 
   message.channel.sendEmbed(bergy);
 };
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["total", "toplamüye", "toplamkişi", "totalmember"],
+  aliases: [],
   permLevel: 0
 };
 
 exports.help = {
   name: "say",
-  description: "Sunucudaki ve Ses Kanallarındaki Kişi Sayısını Gösterir! ",
+  description: "${prefix}say",
   usage: "say"
 };
+//By Bergy#9999
