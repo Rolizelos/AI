687cb422839edca3c1a97ce1704b37480730c79e
Glitch (libercode) <none>
2020-01-23 00:32:09 +0000

    🎷🍘 Checkpoint ./bot.js:8072212/71

--- bot.js
+++ bot.js
@@ -177,6 +177,7 @@ const gecen = moment.duration(zaman1).format(`DD **[Gün,]** HH **[Saat,]** mm *
   .setDescription(`${client.emojis.get("665930842786365473")}Hoşgeldin ${member} Seninle Beraber ${message.guild.memberCount} Kişiyiz! \n${client.emojis.get("665930842786365473")}Kaydının Yapılması İçin sesli odaya geçip ses vermen gerekli! \n${client.emojis.get("665930842786365473")}Hesap Kuruluş Zamanı: ${gecen} \n${client.emojis.get("665930842786365473")}Bu Kullanıcı: \`${cfxzaman}\` \n${client.emojis.get("665930842786365473")}<@&666656114095554560> Rolündeki Yetkililer Seninle İlgilenecektir! `)
   .setFooter(message.guild.name, message.guild.iconURL)
   message.send(bergy)
+  message.send(`${${member}}`)
 
 })
 
