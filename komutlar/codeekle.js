ae3039703a51a12df6c27cbb094450b07ab1cefa
Glitch (libercode) <none>
2020-01-23 12:38:19 +0000

    🐊⭐️ Checkpoint ./komutlar/codeekle.js:8072212/1992

--- komutlar/codeekle.js
+++ komutlar/codeekle.js
@@ -1,26 +1,26 @@
-const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
-    let kanal = args.slice(0).join(' ');
-    let guild = message.guild;
+
+
+
+      message.guild.createChannel(kanal, 'text')}
     if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply('Bunun için gerekli iznin yok'); // Bunu Başkasıda Kullana Bilmesi İçin Kanalları Yönet Yetkisi Verin
     if (kanal.length < 1) return message.reply('Lütfen oluşturacağım kanalın adını yaz.');
-  message.delete();
-  const category = message.guild.channels.find('name', 'JavaScript')
-  guild.createChannel(kanal, 'text')
-
-  message.channel.send("Ses Kanalı Oluşturuldu")
-};
-
-exports.conf = {
+    let guild = message.guild;
+    let kanal = args.slice(0).join(' ');
+  aliases: ['ses-kanal-aç'],
+  description: 'Bir ses kanalı açar',
   enabled: true,
   guildOnly: false,
-  aliases: ['ses-kanal-aç'],
-  permLevel: 3
-};
-
-exports.help = {
+  message.channel.send("Ses Kanalı Oluşturuldu")
+  message.delete();
   name: 'aç',
-  description: 'Bir ses kanalı açar',
+  permLevel: 3
   usage: 'ses-kanal-aç [açmak istediğiniz kanalın adı]'
+ if(message.guild.channels.find('name', 'JavaScript')) {
+const Discord = require('discord.js');
+exports.conf = {
+exports.help = {
+exports.run = (client, message, args) => {
 };
+};
+};---------------
