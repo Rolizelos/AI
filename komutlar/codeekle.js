266c415f7cdd47132e789c9c595c11d505811012
Glitch (libercode) <none>
2020-01-23 12:26:19 +0000

    🌏✍️ Checkpoint ./komutlar/codeekle.js:8072212/3811

--- komutlar/codeekle.js
+++ komutlar/codeekle.js
@@ -1,56 +1,26 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-  
+    let kanal = args.slice(0).join(' ');
+    let guild = message.guild;
+    if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.reply('Bunun için gerekli iznin yok'); // Bunu Başkasıda Kullana Bilmesi İçin Kanalları Yönet Yetkisi Verin
+    if (kanal.length < 1) return message.reply('Lütfen oluşturacağım kanalın adını yaz.');
+  message.delete();
+  const category = message.guild.channels.find('name', 'JavaScript')
+  guild.createChannel(kanal, 'text')
 
-  //KOD BÖLÜMÜ
-  
-
-    if(message.guild.channels.find('name', 'Talepler')) {
-      message.guild.createChannel(`destek-${message.author.id}`, "text").then(c => {
-      const category = message.guild.channels.find('name', 'Talepler')
-      c.setParent(category.id)
-      let role = message.guild.roles.find("name", "Destek Ekibi");
-      let role2 = message.guild.roles.find("name", "@everyone");
-      c.overwritePermissions(role, {
-          SEND_MESSAGES: false,
-          READ_MESSAGES: false
-      });
-      c.overwritePermissions(role2, {
-          SEND_MESSAGES: false,
-          READ_MESSAGES: false
-      });
-      c.overwritePermissions(message.author, {
-          SEND_MESSAGES: true,
-          READ_MESSAGES: true
-      });
-
-      const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setAuthor(`${client.user.username} | Destek Sistemi`)
-      .addField(`Merhaba ${msg.author.username}!`, `Destek Yetkilileri burada seninle ilgilenecektir. \nDestek talebini kapatmak için \`${prefix}kapat\` yazabilirsin.`)
-      .addField(`» Talep Konusu/Sebebi:`, `${message.content}`, true)
-      .addField(`» Kullanıcı:`, `<@${msg.author.id}>`, true)
-      .setFooter(`${client.user.username} | Destek Sistemi`)
-      .setTimestamp()
-      c.send({ embed: embed });
-      c.send(`<@${message.author.id}> Adlı kullanıcı "\`${msg.content}\`" sebebi ile destek talebi açtı! Lütfen Destek Ekibini bekle, @here`)
-      message.delete()
-      }).catch(console.error);
-    }
-  }
+  message.channel.send("Ses Kanalı Oluşturuldu")
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["yaz"],
-  kategori: "Sistem",
-  permLevel: 0
+  aliases: ['ses-kanal-aç'],
+  permLevel: 3
 };
 
 exports.help = {
-  name: "kodekle",
-  description: "Bota yazıldığı kanala mesaj attırır.",
-  usage: "yazdır"
+  name: 'aç',
+  description: 'Bir ses kanalı açar',
+  usage: 'ses-kanal-aç [açmak istediğiniz kanalın adı]'
 };
