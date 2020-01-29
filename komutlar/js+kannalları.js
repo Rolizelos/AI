1ae46d04948e0cc10f02e9a8d2b133404727081c
Glitch (libercode) <none>
2020-01-29 20:43:16 +0000

    🌚🍽 Checkpoint ./komutlar/js+kannalları.js:8072212/589

---
+++ komutlar/js+kannalları.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+exports.run = function(client, message, args) {
+  
+  const js = new Discord.RichEmbed()
+  .setAuthor(`JS+ Kategorisindeki Kanallar`)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ping',
+  description: 'Botun pingini gösterir.',
+  usage: 'ping'
+};
