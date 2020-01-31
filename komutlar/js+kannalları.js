c5b9831167c01dda12d6bc51ca2e3709840b718d
Glitch (libercode) <none>
2020-01-31 21:29:48 +0000

    👩‍❤️‍💋‍👩😚 Checkpoint ./komutlar/js+kannalları.js:8072212/3525

--- komutlar/js+kannalları.js
+++ komutlar/js+kannalları.js
@@ -3,8 +3,8 @@ exports.run = function(client, message, args) {
   
   message.channel.bulkDelete(1)
     let a = message.guild.channels.find(x => x.id === '669657282065465371')
-    let b = a.children.map(x => x)
-  
+    let b = a.children.map(x => x + x.name)
+
   const js = new Discord.RichEmbed()
   .setAuthor(`JS+ Kategorisindeki Kanallar:`)
   .setDescription(`${b.join('\n')}`)
