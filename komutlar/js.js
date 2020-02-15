f3f999653e26132aaf3d3d37ff5ee1f53ba874a8
Glitch (libercode) <none>
2020-02-15 12:00:58 +0000

    🚨🚍 Checkpoint ./komutlar/js.js:8072212/92

--- komutlar/js.js
+++ komutlar/js.js
@@ -2,13 +2,13 @@ const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json')
 exports.run = function(client, message) {
 
-  if(message.channel.id != "669655941397872680") return message.channel.send(':x: **Bu Komutudu <#669655941397872680> Kanalında Kullan!**').then(n => n.delete(5000));
+  if(message.channel.id != "678165777819369482") return message.channel.send(':x: **Bu Komutudu <#678165777819369482> Kanalında Kullan!**').then(n => n.delete(5000));
   
   message.channel.bulkDelete(1)
 
 /////////////////////Config
 
- var logkanali = "true"; //log kanalı kullanılacak ise true yapın kullanılmayacaksa false yapın
+ var logkanali = "false"; //log kanalı kullanılacak ise true yapın kullanılmayacaksa false yapın
  var rolismi = "JavaScript" // Verilecek Rol Ismi
 var logkanalid = "669671496532230145"   
   var guildid = "669638506909401118" // Kullanılacak Sunucu
@@ -18,7 +18,7 @@ message.channel.bulkDelete(1)
   var role = message.guild.roles.find(i => i.name === rolismi); 
   if (message.member.roles.has(role.id)) return message.channel.send("❌ Maalesef, Bu Role Sahipsin").then(n => n.delete(5000));
   if(!role) return message.channel.send(rolismi + "Adından Bir Rol Bulamıyorum Lütfen <@"+ayarlar.sahip+"> 'e ulaş" )
-  if(logkanali == "true") {
+  if(logkanali == "false") {
 message.member.addRole(role);
 message.channel.send("✅  Başarıyla `JavaScript` Rolün verildi!").then(n => n.delete(5000));
 client.channels.get(logkanalid).send("✅  | " + message.author +" Adlı Kullanıcı Başarılı Bir Şekilde **"+rolismi+"** Adlı Rolü Aldı!")
