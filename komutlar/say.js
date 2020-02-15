5d39de975c2ff0106fd5cd3c59ecc40d7f57ae50
Glitch (libercode) <none>
2020-02-15 11:14:44 +0000

    🏝🌹 Checkpoint ./komutlar/say.js:8072212/267

--- komutlar/say.js
+++ komutlar/say.js
@@ -10,9 +10,9 @@ ${client.emojis.get("665930842786365473")} | **Sunucudaki \`Toplam Üye\` Sayıs
 ${client.emojis.get("667740719351463936")} | **Sunucudaki \`Online\` Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'online').size}\`
 ${client.emojis.get("667740716813910037")} | **Sunucudaki \`idle\` Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'idle').size}\`
 ${client.emojis.get("667740717929594950")} | **Sunucudaki \`Dnd\` Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'dnd').size}\`
-${client.emojis.get("669699665532420112")} | **Sunucudaki \`Offline\` Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'offline').size}\`
-${client.emojis.get("669699663523479572")} | **Sunucudaki \`Bot\` Sayısı:** \`${message.guild.members.filter( member => member.user.bot).size}\`
+${client.emojis.get("667740715970854913")} | **Sunucudaki \`Offline\` Sayısı:** \`${message.guild.members.filter(x => x.user.presence.status === 'offline').size}\`
 `)
+  .setFooter(`Komutu Kullanan: ${message.author.tag}`, message.author.avatarURL)   
   message.channel.sendEmbed(bergy);
 };
 exports.conf = {
