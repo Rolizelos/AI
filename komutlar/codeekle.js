0af0611faf8580d1644e568a75c53b18c34dbaf9
Glitch (libercode) <none>
2020-03-15 16:58:34 +0000

    👴🛬 Checkpoint ./komutlar/codeekle.js:8072212/1199

--- komutlar/codeekle.js
+++ komutlar/codeekle.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   message.channel.bulkDelete(1)
   
-   if (!message.member.roles.find('id', '678178183698317315')) return message.channel.send('Bu Kodu Kullanabilmek İçin \`\`SUPPORT\`\` Rolüne Sahip Olmalısın!');
+   if (!message.member.roles.find('id', '678165769963700224')) return message.channel.send('Bu Kodu Kullanabilmek İçin \`\`Code Sharer\`\` Rolüne Sahip Olmalısın!');
   
   
-   let kanal = args[0]
+   let kanal = '📁'+args[0]
   let code = args.slice(1).join(' ');
   if (!kanal) return message.channel.send("Kanal İsmi Yazmalısın!");
   if (!code) return message.channel.send("Kodunu Yazmalısın!");
@@ -17,7 +17,7 @@ message.guild.createChannel(kanal, {type: "text", parent: message.guild.channels
 
 
 `**ARK SUNUCUSU KOD PAYLAŞIM KANALI!**\n
-**${message.author} Tarafından \`\`${kanal}\`\` İsimli Kod Eklendi! ${message.author} Teşşekkür Ederiz! :heart:\n\n
+**${message.author} Tarafından \`\`${kanal}\`\` İsimli Kod Eklendi! ${message.author} Teşekkür Ederiz! :heart:\n\n
 ${client.emojis.get("678184744537686017")} İşte Kod;**
 
 
@@ -27,16 +27,29 @@ ${code}
 **:heart: Sayın Kullanıcılar Kodda Herhangi Bir Hata Varsa <#678169836832030720> Kanalı Yardımıyla Yetkililere İletin!\n:heart: İyi Günler!**`))
 
   
-message.reply(`${client.emojis.get("678184744537686017")} **Kodunuz Başarıyla Eklendi!\n Eklenen Kanalın İsmi:** \`\`${kanal}\`\` `).then(n => n.delete(5000));
+message.reply(`${client.emojis.get("688789528856625165")} **Kodunuz Başarıyla Eklendi!\n Eklenen Kanalın İsmi:** \`\`${kanal}\`\` `)
 } else {
-return message.reply("\`●▬▬▬▬๑「JavaScript」๑▬▬▬▬▬●\` adında bir kategori yok!")
+return message.reply("<#688788941310001199> adında bir kategori yok!")
+   if (message.content.toLowerCase().startsWith(prefix + `kapat`)) {
+    if (!message.channel.name.startsWith(`destek-`))
+      return message.channel.send(`Bu komut sadece Destek Talebi kanallarında kullanılabilir!`);
 
-    
-  
+    var deneme = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setAuthor(`Destek Talebi Kapatma İşlemi`)
+      .setDescription(`Destek talebini kapatmayı onaylamak için, \n10 saniye içinde \`evet\` yazınız.`)
+      .setFooter(`${client.user.username} | Destek Sistemi`);
+    message.channel.send(deneme).then(m => {
+      message.channel
+        .awaitMessages(response => response.content === "evet", {
+          max: 1,
+          time: 10000,
+          errors: ["time"]
+        })
+        .then(collected => {
+          message.channel.delete();
 
-  
- }
-};
+})};
 
 exports.conf = {
   enabled: true,
