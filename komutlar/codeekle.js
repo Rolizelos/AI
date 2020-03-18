59af6b7816f9b6e83c91a4ffeb35e91250ce44e1
Glitch (kod-sunucuv2) <none>
2020-03-18 14:32:14 +0000

    💗🌂 Checkpoint ./komutlar/codeekle.js:8072212/7833 ./komutlar/atatürk.js:8072212/1271

--- komutlar/codeekle.js
+++ komutlar/codeekle.js
@@ -1,50 +1,136 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const db = require("quick.db");
 
-exports.run = (client, message, args) => {
-  message.channel.bulkDelete(1)
+exports.run = async (client, message, args) => {
+if(!message.guild.member(message.author).roles.has('688759558231031941')) return message.reply('Bunu kullanmaya yetkin yok')
   
-   if (!message.member.roles.find('id', '678165769963700224')) return message.channel.send('Bu Kodu Kullanabilmek İçin \`\`Code Sharer\`\` Rolüne Sahip Olmalısın!');
-  
-  
-   let kanal = '📁'+args[0]
-  let code = args.slice(1).join(' ');
-  if (!kanal) return message.channel.send("Kanal İsmi Yazmalısın!");
-  if (!code) return message.channel.send("Kodunu Yazmalısın!");
-  message.delete();
-if (message.guild.channels.find(a => a.id === "688788941310001199")) {
-message.guild.createChannel(kanal, {type: "text", parent: message.guild.channels.find(a => a.id === "688788941310001199")}).then(c => c.send(new Discord.RichEmbed()
-                                                                                                                                             
+  let log = await db.fetch(`kodlog${message.guild.id}`)
+  let cfxlog = message.mentions.channels.first()
+   
+if (args[0] == 'logkapat') {    
+  db.delete(`kodlog${message.guild.id}`)
+  message.channel.send(`Log Kapatıldı.`)
+  return;
+}
+if (args[0] == 'logayarla') {  
+  db.set(`kodlog${message.guild.id}`, cfxlog.id)
+  message.channel.send(`Log ${cfxlog} olarak ayarlandı.`)
+  return;
+}
 
-.setColor("DAR")
-.setFooter(message.guild.name, message.guild.iconURL)
-.setDescription(
-`**ARK SUNUCUSU KOD PAYLAŞIM KANALI!**\n
-**${message.author} Tarafından \`\`${kanal}\`\` İsimli Kod Eklendi! ${message.author} Teşekkür Ederiz! :heart:\n\n
-${client.emojis.get("678184744537686017")} İşte Kod;**
+  if(args[0] == 'paylaş' && args[1] == 'basit'){
+  let JS = message.guild.channels.find(x => x.id === "688788941310001199");
+  message.guild.createChannel(args[2], "text").then(async JSCODE => {  
+  let code = args.slice(3).join(' ');
+  const cfx = new Discord.RichEmbed() 
+  let logkanal = await db.fetch(`kodlog${message.guild.id}`)
+  let kanalog = message.guild.channels.find('id', logkanal)  
+  let gonderen = message.author  
 
+  let role1 = message.guild.roles.find(x => x.name === "Kullanıcı");
+  let role2 = message.guild.roles.find(x => x.name === "@everyone");        
+      JSCODE.overwritePermissions(role2, {SEND_MESSAGES: false,READ_MESSAGES: false});
+      JSCODE.overwritePermissions(role1, {SEND_MESSAGES: false,READ_MESSAGES: true});   
+      JSCODE.setParent(JS)
+      cfx.setDescription(`
+\`${gonderen.tag}\`; \n
+\`${args[2]}\` Kanalını oluşturdum ve belirttiğin kodu kanalın içine attım. \n
+${JSCODE}`)
+      cfx.setFooter(`© FENIX Development`)
+      cfx.setColor("#00ff88")
+      cfx.setTitle('Basit')
 
-${code}
-
-
-**:heart: Sayın Kullanıcılar Kodda Herhangi Bir Hata Varsa <#678169836832030720> Kanalı Yardımıyla Yetkililere İletin!\n:heart: İyi Günler!**`)))
-
-  
-message.reply(`${client.emojis.get("688789528856625165")} **Kodunuz Başarıyla Eklendi!\n Eklenen Kanalın İsmi:** <#${kanal}> `)
-} else {
-return message.reply("<#688789268411580442> adında bir kategori yok!")
-
-     
-}};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'kodekle',
-  description: 'by bergy',
-  usage: 'by bergy'
-};
+      message.channel.sendEmbed(cfx); 
+db.set(`codekanal${message.guild.id}`, JSCODE.id)
+let kanal = await db.fetch(`codekanal${message.guild.id}`)
+let kanalg = message.guild.channels.find('id', kanal)
+kanalg.send(code)
+kanalog.send(new Discord.RichEmbed()
+  .setTitle('Basit')
+  .setDescription(`
+\`${gonderen.tag}\` adlı yetkili tarafından **basit** kodlar kategorisinde,
+` + JSCODE + ` kanalı oluşturuldu ve içerisine yeni kod eklendi.`)
+  .setFooter(`© FENIX Development`)
+  .setColor("#00ff88")) 
+})
+return;
+}
+  if(args[0] == 'paylaş' && args[1] == 'orta'){
+  let JS = message.guild.channels.find(x => x.id === "688789268411580442");
+  message.guild.createChannel(args[2], "text").then(async JSCODE => {  
+  let code = args.slice(3).join(' ');
+  const cfx = new Discord.RichEmbed() 
+  let logkanal = await db.fetch(`kodlog${message.guild.id}`)
+  let kanalog = message.guild.channels.find('id', logkanal)  
+  let gonderen = message.author  
+  let role1 = message.guild.roles.find(x => x.id === "678584042786127881");
+  let role1_1 = message.guild.roles.find(x => x.id === "678368381136666635");
+  let role2 = message.guild.roles.find(x => x.name === "@everyone");        
+      JSCODE.overwritePermissions(role2, {SEND_MESSAGES: false,READ_MESSAGES: false});
+      JSCODE.overwritePermissions(role1, {SEND_MESSAGES: false,READ_MESSAGES: true});
+      JSCODE.overwritePermissions(role1_1, {SEND_MESSAGES: false,READ_MESSAGES: true});
+      JSCODE.overwritePermissions(gonderen, {SEND_MESSAGES: true,READ_MESSAGES: true});
+      JSCODE.setParent(JS)
+      cfx.setDescription(`
+\`${gonderen.tag}\`; \n
+\`${args[2]}\` Kanalını oluşturdum ve belirttiğin kodu kanalın içine attım. \n
+${JSCODE}`)
+      cfx.setFooter(`© FENIX Development`)
+      cfx.setColor("#fff000")
+      cfx.setTitle('Orta seviye')
+      message.channel.sendEmbed(cfx); 
+db.set(`codekanal${message.guild.id}`, JSCODE.id)
+let kanal = await db.fetch(`codekanal${message.guild.id}`)
+let kanalg = message.guild.channels.find('id', kanal)
+kanalg.send(code)
+kanalog.send(new Discord.RichEmbed()
+  .setTitle('Orta seviye')
+  .setDescription(`
+\`${gonderen.tag}\` adlı yetkili tarafından **orta seviye** kodlar kategorisinde,
+` + JSCODE + ` kanalı oluşturuldu ve içerisine yeni kod eklendi.`)
+  .setFooter(`© FENIX Development`)
+  .setColor("#fff000"))
+  })
+return;
+}
+  if(args[0] == 'paylaş' && args[1] == 'zor'){
+  let JS = message.guild.channels.find(x => x.id === "689808695730896919");
+  message.guild.createChannel(args[2], "text").then(async JSCODE => {  
+  let code = args.slice(3).join(' ');
+  const cfx = new Discord.RichEmbed() 
+  let logkanal = await db.fetch(`kodlog${message.guild.id}`)
+  let kanalog = message.guild.channels.find('id', logkanal)  
+  let gonderen = message.author  
+  let role1 = message.guild.roles.find(x => x.id === "678368642093809694");
+  let role1_1 = message.guild.roles.find(x => x.id === "678584251523923997");
+  let role2 = message.guild.roles.find(x => x.name === "@everyone");        
+      JSCODE.overwritePermissions(role2, {SEND_MESSAGES: false,READ_MESSAGES: false});
+      JSCODE.overwritePermissions(role1, {SEND_MESSAGES: false,READ_MESSAGES: true});
+      JSCODE.overwritePermissions(role1_1, {SEND_MESSAGES: false,READ_MESSAGES: true});
+      JSCODE.overwritePermissions(gonderen, {SEND_MESSAGES: true,READ_MESSAGES: true});
+      JSCODE.setParent(JS)
+      cfx.setDescription(`
+\`${gonderen.tag}\`; \n
+\`${args[2]}\` Kanalını oluşturdum ve belirttiğin kodu kanalın içine attım. \n
+${JSCODE}`)
+      cfx.setFooter(`© FENIX Development`)
+      cfx.setColor("#ff0000")
+      cfx.setTitle('Zor seviye')
+      message.channel.sendEmbed(cfx); 
+db.set(`codekanal${message.guild.id}`, JSCODE.id)
+let kanal = await db.fetch(`codekanal${message.guild.id}`)
+let kanalg = message.guild.channels.find('id', kanal)  
+kanalg.send(code)
+kanalog.send(new Discord.RichEmbed()
+  .setTitle('Zor seviye')
+  .setDescription(`
+\`${gonderen.tag}\` adlı yetkili tarafından **zor seviye** kodlar kategorisinde,
+` + JSCODE + ` kanalı oluşturuldu ve içerisine yeni kod eklendi.`)
+  .setFooter(`© FENIX Development`)
+  .setColor("#ff0000"))
+  })
+return;
+}
+  }
+exports.conf = { enabled: true, guildOnly: false, aliases: [], permLevel: 4 };
+exports.help = { name: 'kod' };
