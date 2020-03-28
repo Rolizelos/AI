d4150b6e359d03cd30897d9fe2b6ca51fa191e42
Glitch (kod-sunucuv2) <none>
2020-03-28 21:26:06 +0000

    🏔🍯 Checkpoint ./komutlar/codeekle+.js:8072212/2462 ./komutlar/kodekle.js:8072212/25

--- komutlar/kodekle.js
+++ komutlar/kodekle.js
@@ -1,20 +1,23 @@
 const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json')
 
 exports.run = (client, message, args) => {
 message.channel.bulkDelete(1)
 if (!message.member.roles.find('id', '693562539891621938')) return message.channel.send('Bu Kodu Kullanabilmek İçin <@693562539891621938> Rolüne Sahip Olmalısın!');
-  
+let prefix = ayarlar.prefix
   if (!args[0]) return message.channel.send(new Discord.RichEmbed()
 .setColor("GOLD")
 .setAuthor(`${message.guild.name} Kod Ekleme Sistemi!`)                                            
 .setDescription(`Kodlarınızı Ekleyebileceğiniz \`3\` Adet Kategori Bulunmaktadır!`)
 .addField(`**__Kullanım:__**`, 
-`${prefix}kodekl`)                                            
+`${prefix}kodekle js <kod_ismi> <kodunuz (İsterseniz Açıklama)>
+${prefix}kodekle js+ <kod_ismi> <kodunuz (İsterseniz Açıklama)>
+${prefix}kodekle altyapı <kod_ismi> <altyapı link! (İsterseniz Açıklama)>`)                                            
 )
   
 if(args[0] == 'js') {
-let kanal = '📁'+args[0]
-let code = args.slice(1).join(' ');
+let kanal = '📁'+args[1]
+let code = args.slice(2).join(' ');
 if (!kanal) return message.channel.send("Kanal İsmi Yazmalısın!");
 if (!code) return message.channel.send("Kodunu Yazmalısın!");
 message.delete();
@@ -31,14 +34,14 @@ ${client.emojis.get("693533313180368945")} İşte Kod;**
 ${code}`))
 message.reply(`${client.emojis.get("693533190580731905")} **Kodunuz Başarıyla JavaScript Kategorisine Eklendi!\n Eklenen Kanalın İsmi:** \`\`${kanal}\`\` `).then(n => n.delete(5000));
 } else {
-return message.reply("<#693559264652034209> adında bir kategori yok!")
+return message.reply("<#693559264652034209> adında bir kategori yok!") 
 //---------------------------------------------------------------------------------------------------------------------------  
 //---------------------------------------------------------------------------------------------------------------------------  
 if(args[0] == 'js+') {
-let kanal = '📁'+args[0]
-let code = args.slice(1).join(' ');
-if (!kanal) return message.channel.send("Kanal İsmi Yazmalısın!");
-if (!code) return message.channel.send("Kodunu Yazmalısın!");
+let kanal1 = '📁'+args[1]
+let code1 = args.slice(2).join(' ');
+if (!kanal1) return message.channel.send("Kanal İsmi Yazmalısın!");
+if (!code1) return message.channel.send("Kodunu Yazmalısın!");
 message.delete();
 if (message.guild.channels.find(a => a.id === "693564774885228595")) {
 message.guild.createChannel(kanal, {type: "text", parent: message.guild.channels.find(a => a.id === "693564774885228595")}).then(c => c.send(
@@ -55,10 +58,10 @@ return message.reply("<#693564774885228595> adında bir kategori yok!")
 //---------------------------------------------------------------------------------------------------------------------------  
 //---------------------------------------------------------------------------------------------------------------------------  
 if(args[0] == 'altyapı') {
-let kanal = '📁'+args[0]
-let code = args.slice(1).join(' ');
-if (!kanal) return message.channel.send("Kanal İsmi Yazmalısın!");
-if (!code) return message.channel.send("Kodunu Yazmalısın!");
+let kanal2 = '📁'+args[1]
+let code2 = args.slice(2).join(' ');
+if (!kanal2) return message.channel.send("Kanal İsmi Yazmalısın!");
+if (!code2) return message.channel.send("Kodunu Yazmalısın!");
 message.delete();
 if (message.guild.channels.find(a => a.id === "693564894041210913")) {
 message.guild.createChannel(kanal, {type: "text", parent: message.guild.channels.find(a => a.id === "693564894041210913")}).then(c => c.send(
@@ -71,10 +74,6 @@ ${code}`))
 message.reply(`${client.emojis.get("693533190580731905")} **Kodunuz Başarıyla Altyapı Kategorisine Eklendi!\n Eklenen Kanalın İsmi:** \`\`${kanal}\`\` `).then(n => n.delete(5000));
 } else {
 return message.reply("<#693564894041210913> adında bir kategori yok!")    
-    
-  
-
-  
 
 }}}}}}};
 
