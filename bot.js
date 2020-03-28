ee4e43eda3b30aae33ff76e5a6cb5e4a8aed0e9a
Glitch (kod-sunucuv2) <none>
2020-03-28 22:25:07 +0000

    🎻💘 Checkpoint ./bot.js:8072212/51

--- bot.js
+++ bot.js
@@ -126,7 +126,7 @@ client.on("guildMemberAdd", async member => {
 
 //---------------------------------
 client.on("guildMemberAdd", async member => {
-member.send("Seni Aramızda Görmekten Mutluluk Duyuyoruz! <#693532845175734332> Kanalında `!js` Yazarak JavaScript Rolüne Sahip Olabilirsin! Tekrardan <#693532845175734332> Kanalında `!roller` Yazarak İnvite İle Alabileceğin Rolleri Görebilirsin! İnşallah Seversin Sunucumuzu <3")
+member.send("Seni Aramızda Görmekten Mutluluk Duyuyoruz! <#693532845175734332> Kanalında `!js` Yazarak JavaScript Rolüne Sahip Olabilirsin! Tekrardan <#693532845175734332> Kanalında `!roller` Yazarak İnvite İle Alabileceğin Rolleri Görebilirsin! İnşallah Seversin Sunucumuzu <3 Junex Development Ekibi!")
 })
 
 client.login(process.env.TOKEN);
