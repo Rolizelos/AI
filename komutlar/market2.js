bb7a28b438e1a4d5545cd597972d7f64d36387ac
Glitch (kod-sunucuv2) <none>
2020-04-02 12:26:30 +0000

    🏄🍰 Checkpoint ./komutlar/market.js:8072212/1686 ./komutlar/market2.js:8072212/3067

---
+++ komutlar/market2.js
@@ -0,0 +1,27 @@
+aconst Discord = require('discord.js');
+const db = require('quick.db');
+// ONLY CODE
+exports.run = async (client, message, args) => {
+  let istek = args[0]
+  let user = message.mentions.users.first() || message.member;
+  let gold = await db.fetch(`gold${message.member.id}`);
+  let para = await db.fetch(`para${message.author.id}`);
+  
+ let eklenecekpara = args[1]
+
+message.channel.send("amk ağla")
+  db.add(`para${message.guild}`)}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0,
+
+};
+
+exports.help = {
+  name: 'adminpuan'
+};
+
+// ONLY CODEp
